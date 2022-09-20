b5916dcec2073eeb354f35c1b91d04d629779cbb
qod3r <primculov@protonmail.ch>
2022-09-20 21:00:33 +0700

    lab1

---
+++ lab1/schedule.js
@@ -0,0 +1,14 @@
+let body = document.body;
+let tbl = document.createElement('table');
+tbl.style.border = '1px solid black';
+
+let data = [
+  {
+    "title": "БД",
+    "time": "11:45-13:20"
+  }
+]
+
+
+
+body.appendChild(tbl);
